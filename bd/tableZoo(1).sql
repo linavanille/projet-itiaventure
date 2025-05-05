6e39dc7a60c6ddd11134ab6074c2bdf8cec53d8d
nsourdrille <nathan.sourdrille@insa-rouen.fr>
2025-05-05 18:21:01 +0200

    connection à la base sql

---
+++ bd/tableZoo(1).sql
@@ -0,0 +1,28 @@
+CREATE TABLE Zoo (
+Animal NUMERIC(4),
+Nom VARCHAR (20),
+Annee_naissance NUMERIC(4),
+Espece VARCHAR(10),
+Gardien VARCHAR (20),
+Prenom VARCHAR (10),
+Salaire NUMERIC (10,2),
+Classe VARCHAR (10),
+Origine VARCHAR (10),
+Emplacement NUMERIC(4),
+Surface NUMERIC (4),
+Type_empl NUMERIC(2),
+Libelle_empl VARCHAR (20));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ bd/tuplesZoo.sql
@@ -0,0 +1,37 @@
+--Animal Number(4), Nom VARCHAR2 (20), Annee\_naissance NUMBER(4), Espece VARCHAR2(10), Gardien VARCHAR2 (20), Prenom VARCHAR2 (10),
+--Salaire NUMBER (10,2), Classe VARCHAR2 (10), Origine VARCHAR2 (10), Emplacement NUMBER(4), Surface NUMBER (3), Type\_empl NUMBER(2), 
+--Libelle\_empl VARCHAR2 (20))
+
+INSERT INTO ZOO 
+        VALUES (1, 'Martin', 1985 , 'Ours', 'Lance', 'Pierre',1000, 'mammifère', 'Amérique', 10, 100, 11, 'Forêt');
+        
+INSERT INTO ZOO 
+        VALUES (2, 'Josiane', 1990 , 'Ours', 'Lance', 'Pierre',1000, 'mammifère', 'Amérique', 10, 500, 11, 'Forêt');        
+        
+INSERT INTO ZOO 
+        VALUES (3, 'Josiane', 1995 , 'Girafe', 'Lance', 'Pierre',1000, 'mammifère', 'Afrique', 20, 1000, 22, 'Savane');
+        
+INSERT INTO ZOO 
+        VALUES (4, 'Martine', 1996 , 'Girafe', 'Lance', 'Pierre',1000, 'batracien', 'Amérique', 20, 1000, 22, 'Savane');
+        
+INSERT INTO ZOO 
+        VALUES (5, 'Jaco', 1945 , 'Perroquet', 'Aidubois', 'Laure',1500, 'oiseaux', 'Amérique', 30, 300, 11, 'Forêt');
+        
+INSERT INTO ZOO 
+        VALUES (6, 'Kiwi', 1985 , 'Perroquet', 'Aidubois', 'Laure',1500, 'oiseaux', 'Amérique', 30, 300, 11, 'Forêt');
+        
+INSERT INTO ZOO 
+        VALUES (7, 'Paco', 2000 , 'Perroquet', 'Aidubois', 'Laure',1500, 'oiseaux', 'Amérique', 30, 300, 11, 'Forêt');
+        
+INSERT INTO ZOO 
+        VALUES (8, 'Leo', 2011 , 'Lion', 'Gamotte', 'Albert',2000, 'mammifère', 'Afrique', 40, 1200, 22, 'Savane');
+        
+INSERT INTO ZOO 
+        VALUES (9, 'Lea', 2012 , 'Lion', 'Gamotte', 'Albert',2000, 'mammifère', 'Afrique', 40, 1200, 22, 'Savane');
+        
+INSERT INTO ZOO 
+        VALUES (10, 'Jules', 2010 , 'Chameau', 'Gamotte', 'Albert',2000, 'mammifère', 'Asie', 50, 1000, 33, 'Désert');
+        
+INSERT INTO ZOO 
+        VALUES (11, 'Julie', 2010 , 'Chameau', 'Gamotte', 'Albert',2000, 'mammifère', 'Asie', 50, 1000, 33, 'Désert');                                                                       
+        
