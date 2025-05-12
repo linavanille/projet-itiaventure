d8c90cd38c37f87d087fe676b39eeeac7831c0bf
Thomas Bonneviale <tbonneviale@iti-mahr213-16.ad.insa-rouen.fr>
2025-05-12 14:20:29 +0200

    fichier sql

--- bd/TABLE/tables.sql
+++ bd/TABLE/tables.sql
@@ -4,43 +4,57 @@ CREATE TABLE Monde(
 	
 CREATE TABLE Porte(
 	nomPorte VARCHAR(20) PRIMARY KEY, 
-	etat VARCHAR(11) CHECK (etat IN('FERME','OUVERT','VEROUILLE','DEVEROUILLE')), 
-	nomMonde VARCHAR(20) REFERENCES Monde(nomMonde) ON DELETE CASCADE
+	etat VARCHAR(11) CHECK (etat IN('FERME','OUVERT','VEROUILLE','DEVEROUILLE')) not null,
+	nomMonde VARCHAR(20) not null, 
+	FOREIGN KEY (nomMonde) REFERENCES Monde(nomMonde) ON DELETE CASCADE
 	); 
 
 CREATE TABLE Piece(
 	nomPiece VARCHAR(20) PRIMARY KEY,
-	nomMonde VARCHAR(20) REFERENCES Monde(nomMonde) ON DELETE CASCADE
+	nomMonde VARCHAR(20) not null,
+	FOREIGN KEY (nomMonde) REFERENCES Monde(nomMonde) ON DELETE CASCADE
 	);
 	
 CREATE TABLE PiedDeBiche(
 	nomPDB VARCHAR(20) PRIMARY KEY, 
-	estDeplacabe BOOL, 
-	nomMonde VARCHAR(20) REFERENCES Monde(nomMonde) ON DELETE CASCADE
+	estDeplacabe BOOL not null, 
+	nommonde VARCHAR(20) not null,
+	FOREIGN KEY (nomMonde) REFERENCES Monde(nomMonde) ON DELETE CASCADE
 	);
 	
 CREATE TABLE JoueurHumain(
 	nomJoueur VARCHAR(20) PRIMARY KEY, 
-	pointVie INTEGER CHECK (pointVie >= 0), 
-	pointForce INTEGER CHECK (pointForce >= 0), 
-	nomPiece VARCHAR(20) REFERENCES Piece(nomPiece) ON DELETE CASCADE,
-	nomMonde VARCHAR(20) REFERENCES Monde(nomMonde) ON DELETE CASCADE
+	pointVie INTEGER CHECK (pointVie >= 0) not null, 
+	pointForce INTEGER CHECK (pointForce >= 0) not null, 
+	nomPiece VARCHAR(20) not null,
+	nomMonde VARCHAR(20) not null,
+	FOREIGN KEY (nomPiece) REFERENCES Piece(nomPiece) ON DELETE CASCADE,
+	FOREIGN KEY (nomMonde) REFERENCES Monde(nomMonde) ON DELETE CASCADE
 	);
 	
 CREATE TABLE PossedePorte(
-	nomPorte VARCHAR(20) REFERENCES Porte(nomPorte) ON DELETE CASCADE, 
-	nomPiece VARCHAR(20) REFERENCES Piece(nomPiece) ON DELETE CASCADE,
-	nomMonde VARCHAR(20) REFERENCES Monde(nomMonde)	ON DELETE CASCADE
+	nomporte VARCHAR(20) not null,
+	nompiece VARCHAR(20) not null,
+	nomMonde VARCHAR(20) not null,
+	FOREIGN KEY (nomPorte) REFERENCES Porte(nomPorte) ON DELETE CASCADE, 
+	FOREIGN KEY (nomPiece) REFERENCES Piece(nomPiece) ON DELETE CASCADE,
+	FOREIGN KEY (nomMonde) REFERENCES Monde(nomMonde)	ON DELETE CASCADE
 	);
 	
 CREATE TABLE ContientPDB(
-	nomPDB VARCHAR(20) REFERENCES PiedDeBiche(nomPDB) ON DELETE CASCADE, 
-	nomPiece VARCHAR(20) REFERENCES Piece(nomPiece) ON DELETE CASCADE,
-	nomMonde VARCHAR(20) REFERENCES Monde(nomMonde) ON DELETE CASCADE	
+	nomPDB VARCHAR(20) not null,
+	nomPiece VARCHAR(20) not null,
+	nomMonde VARCHAR(20) not null,
+	FOREIGN KEY (nomPDB) REFERENCES PiedDeBiche(nomPDB) ON DELETE CASCADE, 
+	FOREIGN KEY (nomPiece) REFERENCES Piece(nomPiece) ON DELETE CASCADE,
+	FOREIGN KEY (nomMonde) REFERENCES Monde(nomMonde) ON DELETE CASCADE	
 	);
 	
 CREATE TABLE PossedePDB(
-	nomPDB VARCHAR(20) REFERENCES PiedDeBiche(nomPDB) ON DELETE CASCADE, 
-	nomJoueur VARCHAR(20) REFERENCES JoueurHumain(nomJoueur) ON DELETE CASCADE,
-	nomMonde VARCHAR(20) REFERENCES Monde(nomMonde)	ON DELETE CASCADE
+	nomPDB VARCHAR(20),
+	nomJoueur VARCHAR(20),
+	nomMonde VARCHAR(20),
+	FOREIGN KEY (nomPDB) REFERENCES PiedDeBiche(nomPDB) ON DELETE CASCADE, 
+	FOREIGN KEY (nomJoueur) REFERENCES JoueurHumain(nomJoueur) ON DELETE CASCADE,
+	FOREIGN KEY (nomMonde) REFERENCES Monde(nomMonde)	ON DELETE CASCADE
 	);
--- bd/TABLE/test_bd.sql
+++ bd/TABLE/test_bd.sql
@@ -1,2 +1,8 @@
 INSERT INTO Monde VALUES ('LeMondeDeLina');
-
+INSERT INTO Piece VALUES ('SalleDeTP','LeMondeDeLina');
+INSERT INTO Porte VALUES ('Porte','FERME','LeMondeDeLina');
+INSERT INTO PiedDeBiche values ('PDB',True, 'LeMondeDeLina');
+INSERT INTO JoueurHumain values ('Lina', 100, 100, 'SalleDeTP','LeMondeDeLina');
+INSERT INTO PossedePorte values ('Porte','SalleDeTP', 'LeMondeDeLina');
+INSERT INTO contientPDB values ('PDB', 'SalleDeTP','LeMondeDeLina');
+INSERT INTO possedePDB values ('PDB','Lina','LeMondeDeLina');
