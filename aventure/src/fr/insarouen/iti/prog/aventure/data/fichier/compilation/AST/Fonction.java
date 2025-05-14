440f3adeefc19dd088b2721dd112b222b08e476d
lelomaribouya <lina.el_omari_bouya@insa-rouen.fr>
2025-05-14 12:09:22 +0200

    implémentation de Fonction.java

--- aventure/src/fr/insarouen/iti/prog/aventure/data/fichier/compilation/AST/Fonction.java
+++ aventure/src/fr/insarouen/iti/prog/aventure/data/fichier/compilation/AST/Fonction.java
@@ -1,1 +1,29 @@
 package fr.insarouen.iti.prog.aventure.data.fichier.compilation.AST;
+
+//Stocker types, arguments, identifiants
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List; 
+import fr.insarouen.iti.prog.aventure.data.fichier.compilation.AST.SuiteArguments;
+import fr.insarouen.iti.prog.aventure.data.fichier.compilation.automate.Identifiant;
+
+public class Fonction{
+	private Identifiant lIdentifiant; 
+	private SuiteArguments lesArguments; 
+	
+	public Fonction(Identifiant lIdentifiant, SuiteArguments lesArguments){
+		this.lIdentifant = lIdentifiant; 
+		this.lesArguements = lesArguments; 
+	}
+	
+	public Identifiant getIdentifiant(){
+        return lIdentifiant;
+    }
+    
+    public Arguments getArguments(){
+    	return lesArguments;
+    }
+
+
+}
