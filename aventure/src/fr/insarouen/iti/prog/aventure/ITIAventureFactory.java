f19d18d6c111c6f4ea75595bef9431301e94e2b1
nsourdrille <nathan.sourdrille@insa-rouen.fr>
2025-05-14 12:55:13 +0200

    début design pattern

---
+++ aventure/src/fr/insarouen/iti/prog/aventure/ITIAventureFactory.java
@@ -0,0 +1,35 @@
+package fr.insarouen.iti.prog.aventure;
+
+import fr.insarouen.iti.prog.aventure.Monde;
+import fr.insarouen.iti.prog.aventure.elements.vivants.Monstre;
+import fr.insarouen.iti.prog.aventure.elements.structure.Porte;
+import fr.insarouen.iti.prog.aventure.elements.structure.Piece;
+import fr.insarouen.iti.prog.aventure.elements.objets.serrurerie.Cle;
+import fr.insarouen.iti.prog.aventure.elements.objets.serrurerie.Serrure;
+
+public class ITIAventureFactory{
+
+    public Monde creationMonde(String factory){
+        return null;
+    }
+
+    public Monstre creationMonstre(String factory){
+        return null;
+    }
+
+    public Piece creationPiece(String factory){
+        return null;
+    }
+
+    public Porte creationPorte(String factory){
+        return null;
+    }
+
+    public Serrure creationSerrure(String factory){
+        return null;
+    }
+
+    public Cle creationCle(String factory){
+        return null;
+    }
+}
