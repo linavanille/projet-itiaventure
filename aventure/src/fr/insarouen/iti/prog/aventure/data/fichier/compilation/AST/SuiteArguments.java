80e1a00c04930e575234ebdb82d93a97c482bc6e
Thomas Bonneviale <tbonneviale@iti-mahr213-16.ad.insa-rouen.fr>
2025-05-14 12:05:07 +0200

    AST suiteArguments

--- aventure/src/fr/insarouen/iti/prog/aventure/data/fichier/compilation/AST/SuiteArguments.java
+++ aventure/src/fr/insarouen/iti/prog/aventure/data/fichier/compilation/AST/SuiteArguments.java
@@ -1,1 +1,18 @@
 package fr.insarouen.iti.prog.aventure.data.fichier.compilation.AST;
+
+import java.util.ArrayList;
+import java.util.List;
+import fr.insarouen.iti.prog.aventire.data.fichier.compilation.automate.ArgumentSimple;
+
+public class SuiteArguments {
+    private List<ArgumentSimple> lesArgs;
+
+    public SuiteArguments(List<ArgumentSimple> desArgs){
+        this.lesArgs = new ArrayList<>();
+        this.lesArgs.addAll(desArgs);
+    }
+
+    public getArguments(){
+        return this.lesArgs;
+    }
+}
