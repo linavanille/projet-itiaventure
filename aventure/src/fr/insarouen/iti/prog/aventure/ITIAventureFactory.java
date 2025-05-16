c871652fa6600ecb0c49e45a3ccc441f2ec46f03
nsourdrille <nathan.sourdrille@insa-rouen.fr>
2025-05-16 10:32:42 +0200

    ITIAventureFactory

--- aventure/src/fr/insarouen/iti/prog/aventure/ITIAventureFactory.java
+++ aventure/src/fr/insarouen/iti/prog/aventure/ITIAventureFactory.java
@@ -9,27 +9,37 @@ import fr.insarouen.iti.prog.aventure.elements.objets.serrurerie.Serrure;
 
 public class ITIAventureFactory{
 
-    public Monde creationMonde(String factory){
-        return null;
+    private Monde monde;
+
+    public Monde creationMonde(String nom){
+        this.monde = new Monde(nom);
+        return this.monde;
     }
 
-    public Monstre creationMonstre(String factory){
-        return null;
+    public Monstre creationMonstre(String nom, int pointsVie, int pointsForce, Piece piece) throws NomDEntiteDejaUtiliseDansLeMondeException {
+        return new Monstre(nom, this.monde, pointsVie, pointsForce, piece);
     }
 
-    public Piece creationPiece(String factory){
-        return null;
+    public Piece creationPiece(String nom)  throws NomDEntiteDejaUtiliseDansLeMondeException  {
+        return new Piece(nom, this.monde);
     }
 
-    public Porte creationPorte(String factory){
-        return null;
+    public Porte creationPorte(String nom, Piece pieceA, Piece pieceB) throws NomDEntiteDejaUtiliseDansLeMondeException{
+        return new Porte(nom, this.monde, pieceA, pieceB);
     }
 
-    public Serrure creationSerrure(String factory){
-        return null;
+    public Porte creationPorteSerrure(String nom, Piece pieceA, Piece pieceB ) throws NomDEntiteDejaUtiliseDansLeMondeException{
+        Serrure serrure = creationSerrure();
+        return new Porte(nom, this.monde, serrure, pieceA, pieceB);
     }
 
-    public Cle creationCle(String factory){
-        return null;
+    public Serrure creationSerrure() throws NomDEntiteDejaUtiliseDansLeMondeException{
+        return new Serrure(this.monde);
+    }
+
+    public void creationCle(String nomPorte, String nomPiece)throws NomDEntiteDejaUtiliseDansLeMondeException {
+        Cle cle =((Porte)this.monde.getEntite(nomPorte)).getSerrure().creerCle();
+        ((Piece)this.monde.getEntite(nomPiece)).deposer(cle);
     }
 }
+
