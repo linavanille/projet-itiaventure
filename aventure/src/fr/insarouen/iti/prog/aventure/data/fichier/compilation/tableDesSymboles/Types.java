95ea29201f6b25bdbbde00c077a9d81a68f96785
lelomaribouya <lina.el_omari_bouya@insa-rouen.fr>
2025-05-19 15:57:19 +0200

    comp

---
+++ aventure/src/fr/insarouen/iti/prog/aventure/data/fichier/compilation/tableDesSymboles/Types.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insarouen.iti.prog.aventure.data.fichier.compilation.tableDesSymboles;
+
+import fr.insarouen.iti.prog.aventure.Monde;
+import fr.insarouen.iti.prog.aventure.elements.Entite;
+/**
+ * Classe énumérée qui définit les types possibles pour une veriable.
+ * @author delestre
+ */
+public class Types {
+
+    private Monde monde;
+    private Entite entite;
+}
