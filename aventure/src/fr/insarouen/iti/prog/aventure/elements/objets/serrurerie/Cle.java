c8153eabf4f71fa47d151f6b65a73992816bba7f
lelomaribouya <lina.el_omari_bouya@insa-rouen.fr>
2025-05-26 20:25:29 +0200

    doc + clear

--- aventure/src/fr/insarouen/iti/prog/aventure/elements/objets/serrurerie/Cle.java
+++ aventure/src/fr/insarouen/iti/prog/aventure/elements/objets/serrurerie/Cle.java
@@ -2,43 +2,32 @@ package fr.insarouen.iti.prog.aventure.elements.objets.serrurerie;
 
 import fr.insarouen.iti.prog.aventure.elements.objets.Objet;
 import fr.insarouen.iti.prog.aventure.Monde;
-
 import fr.insarouen.iti.prog.aventure.NomDEntiteDejaUtiliseDansLeMondeException;
-import fr.insarouen.iti.prog.aventure.EntiteDejaDansUnAutreMondeException;
 
 /**
- * Classe {@code Cle} représentant un objet clé dans le monde de l'aventure.
- * <p>
- * Cette classe hérite de {@link Objet} et représente un objet qui peut être utilisé pour
- * interagir avec des serrures dans le monde du jeu.
- * </p>
+ * Représente une clé dans le monde de l'aventure.
+ * Les clés peuvent interagir avec des serrures et sont toujours déplaçables.
  */
-public class Cle extends Objet{
+public class Cle extends Objet {
 
-    /**
-     * Constructeur de la classe {@code Cle}.
-     * <p>
-     * Le constructeur permet de créer une clé dans un monde donné. 
-     * Si le nom de l'entité est déjà utilisé dans ce monde, une exception sera levée.
-     * </p>
-     * 
-     * @param nom le nom de la clé.
-     * @param monde le monde dans lequel la clé est créée.
-     * @throws NomDEntiteDejaUtiliseDansLeMondeException si le nom de l'entité est déjà utilisé dans le monde.
-     */
-    protected Cle(String nom, Monde monde) throws NomDEntiteDejaUtiliseDansLeMondeException{
-        super(nom,monde);
-    }
+	/**
+	 * Constructeur pour créer une clé.
+	 *
+	 * @param nom Nom de la clé.
+	 * @param monde Monde dans lequel la clé existe.
+	 * @throws NomDEntiteDejaUtiliseDansLeMondeException si le nom est déjà utilisé.
+	 */
+	protected Cle(String nom, Monde monde) throws NomDEntiteDejaUtiliseDansLeMondeException {
+		super(nom, monde);
+	}
 
-    /**
-     * Indique si l'objet clé est déplaçable.
-     * <p>
-     * Les clés sont déplaçables, ce qui signifie qu'elles peuvent être prises et transportées par des entités.
-     * </p>
-     * 
-     * @return {@code true} car une clé est déplaçable.
-     */
-    public boolean estDeplacable(){
-        return true;
-    }
+	/**
+	 * Une clé est toujours déplaçable.
+	 *
+	 * @return true.
+	 */
+	public boolean estDeplacable() {
+		return true;
+	}
 }
+
