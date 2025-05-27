4fc5a1ae5f5ed990ecb05d2cb96425bf45cca1af
lelomaribouya <lina.el_omari_bouya@insa-rouen.fr>
2025-05-27 13:58:53 +0200

    correction

---
+++ aventure/src/fr/insarouen/iti/prog/aventure/data/fichier/compilation/exceptions/TypeDeParametreIncorrectException.java
@@ -0,0 +1,23 @@
+package fr.insarouen.iti.prog.aventure.data.fichier.compilation.exceptions;
+
+import fr.insarouen.iti.prog.aventure.data.fichier.compilation.AST.Identifiant;
+
+/**
+ * Exception levée lorsque durant l'interpétation d'une expression cette 
+ * dernière utilise une variable qui est inconnue. 
+ * @author delestre
+ */
+public class TypeDeParametreIncorrectException extends Exception{
+    private Identifiant idAyantProvoqueException;
+    
+    /**
+     * Constructeur de l'exception à partifir de l'identifiant de la 
+     * variable inconnue.
+     * @param id l'identifiant
+     */
+    public TypeDeParametreIncorrectException(String s) {
+        super(s);
+    }
+
+
+}
